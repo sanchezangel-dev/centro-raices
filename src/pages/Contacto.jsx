b77523227db748d03c657c30ae837642ecae3eff
angelsanchez-dev <angelsanchez0113@gmail.com>
2026-02-01 17:37:38 -0300

    Contacto

--- src/pages/Contacto.jsx
+++ src/pages/Contacto.jsx
@@ -89,7 +89,7 @@ const Contacto = () => {
                     <div className="contacto-column info-side">
                         <div className="map-wrapper">
                             <iframe
-                                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3283.005976515935!2d-58.37159612521136!3d-34.62928925884747!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95a334cb7eee352d%3A0x58a3e8cb662b3bd0!2sIrala%2073%2C%20C1161ADB%20Cdad.%20Aut%C3%B3noma%20de%20Buenos%20Aires!5e0!3m2!1ses!2sar!4v1769310110587!5m2!1ses!2sar"
+                                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3283.005264231254!2d-58.371556225211314!3d-34.6293072588484!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95a334cb7848105d%3A0x5d1a04b1141f1a4e!2sIrala%2075%2C%20C1163%20Cdad.%20Aut%C3%B3noma%20de%20Buenos%20Aires!5e0!3m2!1ses!2sar!4v1769978134283!5m2!1ses!2sar"
                                 width="100%"
                                 height="100%"
                                 style={{ border: 0 }}
@@ -101,7 +101,7 @@ const Contacto = () => {
                         <div className="datos-contacto-rapido">
                             <div className="dato-item">
                                 <i className="fa-solid fa-location-dot"></i>
-                                <p>Irala 73, La Boca, CABA</p>
+                                <p>Irala 75, La Boca, CABA</p>
                             </div>
                             <div className="dato-item">
                                 <i className="fa-brands fa-whatsapp"></i>
